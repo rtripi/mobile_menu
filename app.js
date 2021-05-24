38b4036d364a4c7d507c1e5bfbcef8440718aefe
Renato Tripi <renatosft@gmail.com>
2021-05-24 18:04:48 -0300

    added aria attributes

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ function toggleMenu(event) {
   const nav = document.getElementById("nav");
 
   nav.classList.toggle("active");
+
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Close menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Open menu");
+  }
 }
 
 btnMobile.addEventListener("click", toggleMenu);
